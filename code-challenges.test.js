29db326d5cecb47903795800b47414e7c84b4f99
gnostertag <ostertag2000@gmail.com>
2023-04-03 07:51:56 -0700

    week 3 completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,54 +14,158 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
+describe("fibSequence",()=>{
+  
+  const fibLength1 = 6
+  const fibLength2 = 10
+
+  it("returns an array that contains the numbers of the fibonacci sequence", ()=> {
+
+    expect(fibSequence(fibLength1)).toEqual ([1, 1, 2, 3, 5, 8])
+    expect(fibSequence(fibLength2)).toEqual ([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    
+  })
+})
+// FAIL  ./code-challenges.test.js
+//   fibSequence
+//     ✕ returns an array that contains the numbers of the fibonacci sequence (1 ms)
+
+//   ● fibSequence › returns an array that contains the numbers of the fibonacci sequence
+
+//     TypeError: "fibSequence" is not a function
+
 // Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // b) Create the function that makes the test pass.
+//Pseudo:
+//Input:number greater than 2
+//Output: array
+//we will create a function called fibSequence that will first check if the number is greater than 2. If not it will return an eror. Then we will create a function fibNum that will take in an array of 0 and 1 because 0 and 1 are the always the first two numbers of the fib sequence. next a for loop to calculate the numbers of the fib sequence and store them in an array. Then console log the given fibLength numbers and we should expect to see [ 1, 1, 2, 3, 5, 8 ] and [  1,  1,  2,  3,  5,  8, 13, 21, 34, 55].
+
+const fibSequence = (num) => {
+  if (num < 3) {
+    return ("something went wrong")
+  }
+
+const fibNum = ([1,1]); {
+for (let i =2; i<num; i++){
+  fibNum[i] = fibNum[i-1] +fibNum[i-2];
+}
+return fibNum;
+}}
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
+
+
+describe("lowToHigh",()=>{
+  
+  const studyMinutesWeek1 = {
+    sunday: 90,
+    monday: 30,
+    tuesday: 20,
+    wednesday: 15,
+    thursday: 30,
+    friday: 15,
+    saturday: 60
+  }
+  const studyMinutesWeek2 = {
+    sunday: 100,
+    monday: 10,
+    tuesday: 45,
+    wednesday: 60,
+    thursday: 20,
+    friday: 15,
+    saturday: 65
+  }
+     it("returns an array of the values from least to greatest", ()=> {
+  
+      expect(lowToHigh(studyMinutesWeek1)).toEqual ([15, 15, 20, 30, 30, 60, 90])
+      expect(lowToHigh(studyMinutesWeek2)).toEqual ([10, 15, 20, 45, 60, 65, 100])
+      
+     })
+    })
+
+    // FAIL  ./code-challenges.test.js
+    // lowToHigh
+    //   ✕ returns an array of the values from least to greatest (1 ms)
+  
+    // ● lowToHigh › returns an array of the values from least to greatest
+  
+    //   TypeError: "lowToHigh" is not a function
+
+    
 // Expected output: [15, 15, 20, 30, 30, 60, 90]
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
 // b) Create the function that makes the test pass.
+
+//pseudo:
+//Input: object
+//output: array of the values sorted from least to greatest.
+//Create a function called lowToHigh. Then use the Object.values method to isolate the values from each object. Then we will use the .sort method to put those isolated values in order of least to greatest. We should expect to see [15, 15, 20, 30, 30, 60, 90] and [10, 15, 20, 45, 60, 65, 100] respectively.
+
+
+
+
+const lowToHigh = (obj) => {
+return Object.values(obj).sort((a,b)=>a-b)
+}
+
+
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
+
+
+describe("accuSum",()=>{
+
+  const accountTransactions1 = [100, -17, -23, -9]
+  const accountTransactions2 = [250, -89, 100, -96]
+  const accountTransactions3 = []
+
+  it("returns an array of the accumulating sum", ()=>{
+
+    expect(accuSum(accountTransactions1)).toEqual ([100, 83, 60, 51])
+    expect(accuSum(accountTransactions2)).toEqual ([250, 161, 261, 165])
+    expect(accuSum(accountTransactions3)).toEqual ([])
+  })
+})
+
+// FAIL  ./code-challenges.test.js
+// accumulatingSum
+//   ✕ returns an array of the accumulatingSum (2 ms)
+
+// ● accumulatingSum › returns an array of the accumulatingSum
+
+//   TypeError: "accumulatingSum" is not a function
+
 // Expected output: [100, 83, 60, 51]
 
-const accountTransactions2 = [250, -89, 100, -96]
 // Expected output: [250, 161, 261, 165]
 
-const accountTransactions3 = []
 // Expected output: []
 
 // b) Create the function that makes the test pass.
+//pseudo
+//input: array
+//output: an array of accumulating sums.
+//First im going to create a function called accuSum. Then we'll create a new variable called sum and set that = 0. Its equal to 0 because I want 0 to be th initial value before we use any methods. Next ill use the .map function to iterate over the array and at each iteration i want the current value to be added to the sum. Then ill create a function for of the given arrays and call them to get the expected outputs of [100, 83, 60, 51] , [250, 161, 261, 165] , and [].
+
+
+const accuSum = (arr) => {
+  let sum = 0
+  return arr.map((value) => {
+     return sum+= value
+  })
+}
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX from my understanding allows html syntax to function just like javascript. In order to use JSX we need to use curly braces.
 
-Researched answer:
+Researched answer: JSX is an extnesion for javascript that lets us use html syntax in javascript code. JSX is usually used in react. In JSX we can use the html elements we are familiar with, like div, and h1 in Javascript. One of the benefits of using JSX is that with it we can write code that is easier to debug.
 
 1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: yarn is a package manager that collects all the necessary files we need to create a react app. It is also the command used in the terminal to run a test with jest. I'm not 100% sure what files are modified when we run yarn but i imagine it has something to do with the package files that show up in vscode after we reate a react app;
 
-Researched answer:
+Researched answer:Yarn is an acronym that stands for yet another resource negotiator. It is a package manager that all the code snippets and dependencies necessary for our code. The filed yarn.lock, package.json, node_modules, .yarnc, and .yarn-integrity are all modified when we run yarn in the terminal. Thses are the most common files modified by yarn when ran in the terminal.
 
 3. What is a React component?
 
-Your answer:
+Your answer: A react component is any element of a react app that is interactive on the webpage. For example, if we want to create a navigation bar that the user can click on to get directions, we will need to create a component in our text editor to store that logic.
 
-Researched answer:
+Researched answer: A react Component is a reusable building block for user interface elements. Think of a component as a javascript function that returns a JSX element. Components are either functional components or class based components. Components let us create the UI so that it is easier to test and build.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: The difference between the two is that State values are data that tell us how the component will show up on the browser. Props are how you communicate between the components. Think of it this way, state is used to figure out what shows up on the screen while props are how you pass data between components.
 
-Researched answer:
+Researched answer: Props is short for properties and they are ways to pass data from a parent component to its child component. From the child component you can acess the props and create a separate output. State values is the data inside the component. State values are declared using setState(). State is also what keeps track of the user input.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: DOM stands for document object model. It organizes html elements into a tree. I think of DOM as a visiual representation of HTML. I also think of DOM as a place to make changes before applying them to the web page.
 
-Researched answer:
+Researched answer:DOM (document object model) shows the structure of an HTML document in a node tree. When we are looking at websites in our browser, we are looking at DOM. 
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: A div tag is used in html to organize the code that we write. Similarly a span tag is used to do the same but with smaller sections of code.
 
-Researched answer:
+Researched answer: A div tag is a type of element in html that is used as a container for other elements. div tags usually house headers and footers. div can also be used to apply thisng like classes and id's to larger blocks of code. A span tag is similar to div except it is used to style smaler blocks of code, like single words, or characters. To give an example, span can be used to style certain words in a paragraph, While div would be used to change the font of the entire paragraph.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Also referred to as OOP, object oriented programming is a method that was created to deal with large and complicated software systems. Developers needed a way to change small components withing without affecting the entire code. Often times when working with big projects, small changes can be hard to track, breaking it into small parts can be easier to find bugs.
 
-2. Ruby:
+2. Ruby:Ruby is an object oriented programming language that is designed to be easy to read and write. Ruby was created in the 90s and its strong focus on simplicity makes it a popular language. There are many built in methods in ruby and it is commonly used in the ruby on rails framework.
 
-3. Implicit return:
+3. Implicit return: Ruby is a language that uses implicit returns, this basically means that if there is no specified return, the value of the last method used will be returned. Implicit returns can make code shorter, but I can see how it could make it harder to read.
 
-4. Ruby blocks:
+4. Ruby blocks: A ruby block is a snippet of code that can be passed as an argument to a method. It is an anonymous function and it is usually defined with the key words do and end.
 
-5. Ruby hashes:
+5. Ruby hashes: A ruby hash is a structure that lets us store data in key value pairs. It looks alot like an array but only you can use objects as keys insted of using integers to access elements of a hash.
